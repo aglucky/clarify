741c6fe0bfbf5df6b9cbe35bfe7172788483e676
adam gluck <adam.gluck10@gmail.com>
2024-01-28 16:59:34 -0500

    simplify navbar

--- components/navigation/nav-bar.tsx
+++ components/navigation/nav-bar.tsx
@@ -1,25 +1,23 @@
 'use client'
-import Link from "next/link"
-
-
+import Link from "next/link";
 
 export default function NavBar() {
-    const menuItems = [
-        { label: "Home", href: "/" }
-    ]
-    return (
-        <div className="container mx-auto px-6 md:px-8 lg:px-10 py-2">
-            <div className="flex justify-between items-center">
-                <div className="hidden md:flex space-x-8">
-                    {menuItems.map((item, index) => (
-                        <Link key={index} href={item.href}>
-                            <span className="text-white">
-                                {item.label}
-                            </span>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </div>
-    )
+
+  const menuItems = [
+    { label: "Home", href: "/" },
+  ];
+
+  return (
+    <nav>
+      <ul style={{ listStyle: 'none', margin: 0, padding: 0, display: 'flex' }}>
+        {menuItems.map((item, index) => (
+          <li key={index} style={{ marginRight: 10 }}>
+            <Link href={item.href} style={{ textDecoration: 'none', color: 'black' }}>
+                {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
